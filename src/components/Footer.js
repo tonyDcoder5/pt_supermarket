22012ee628ae792eb358b57c70206b5d38fedaa2
TonyDCoder5 <tonyaflex5@gmail.com>
2023-01-07 23:08:18 -0700

    add fax number, change header img

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,7 @@ export default function Footer() {
   const data = {
     address: "665 W Lancaster Rd, Orlando, FL 32809",
     phone: "407-582-0140",
+    fax: "407-582-0190",
     email: "plazatropical665@gmail.com",
     hours: [
       { day: "SUN", hours: ["8AM", "7PM"] },
@@ -42,8 +43,9 @@ export default function Footer() {
         </div>
         <div id="footer-contact">
           <div className="col contact-info">
-            <h3>{data.address}</h3>
-            <h5>{data.phone}</h5>
+            <p>{data.address}</p>
+            <p>Phone: {data.phone}</p>
+            <p>Fax: {data.fax}</p>
             <p>{data.email}</p>
           </div>
           <div className="socials">
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ export default function Header() {
   const data = {
     address: "665 W Lancaster Rd, Orlando, FL 32809",
     phone: "407-582-0140",
+    fax: "407-582-0190",
     email: "plazatropical665@gmail.com",
     hours: [
       { day: "SUN", hours: ["8AM", "7PM"] },
@@ -27,7 +28,8 @@ export default function Header() {
       <div id="header-contact">
         <div className="col contact-info">
           <p>{data.address}</p>
-          <p>{data.phone}</p>
+          <p>Phone: {data.phone}</p>
+          <p>Fax: {data.fax}</p>
           <p>{data.email}</p>
         </div>
         <div className="socials">
